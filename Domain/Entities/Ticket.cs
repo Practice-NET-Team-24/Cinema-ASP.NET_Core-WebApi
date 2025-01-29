410342b297929932c4e2fb735c35f7bb2ed21a3f
AlexBoiko412 <bojkosasa78@gmail.com>
2025-01-29 16:13:09 +0100

    Defined entities

---
+++ Domain/Entities/Ticket.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+        public int PlaceId { get; set; }
+        public int SessionId { get; set; }
+        public int UserId { get; set; }
+        public Place Place { get; set; }
+        public Session Session { get; set; }
+        public User User { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
